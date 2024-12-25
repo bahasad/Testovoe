5feb5d9c032f6c827980f60f27053703167534c2
Baha Sadyr <>
2024-12-25 04:01:03 -0600

    Initial commit

---
+++ TestingTaskHealthApp/TestingTaskHealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingTaskHealthAppApp.swift
+//  TestingTaskHealthApp
+//
+//  Created by Baha Sadyr on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingTaskHealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
